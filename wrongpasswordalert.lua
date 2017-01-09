fe84b590c342bc8836861f0519952a5699f4d3ed
Phoenix <bugraules@gmail.com>
2017-01-09 09:56:11 +0200

    Update wrongpasswordalert.lua

--- wrongpasswordalert.lua
+++ wrongpasswordalert.lua
@@ -4,7 +4,12 @@ wifi.sta.config("SSID","password",false) -- oto connection closed
 
 wifi.sta.autoconnect(0) --oto connection closed
 
-wifi.sta.connect(false)
---
+wifi.sta.connect(false) --oto connection closed
+
 wifi.sta.eventMonReg(wifi.STA_WRONGPWD, function() print("STATION_WRONG_PASSWORD") end)
+
+ip = wifi.sta.getip() -- assing exist If IP exists to "ip"
+print(ip) --print "ip"
 --done
+
+
