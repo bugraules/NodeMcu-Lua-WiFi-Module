58299aee9862a20372f4831cdb6341ea9d2a5fe2
Phoenix <bugraules@gmail.com>
2017-01-11 15:26:11 +0200

    Update delay.lua

--- delay.lua
+++ delay.lua
@@ -6,3 +6,6 @@ tmr.delay()
 --returns immediately before the output sequence is finalized. HW timer FRC1_SOURCE mode is used to change the states.
 --As there is only a single hardware timer, there are restrictions on which modules can be used at the same time. 
 --An error will be raised if the timer is already in use.
+
+--NOTE: check the SDK structure If you had irregularities of your delays timers ...
+--http://nodemcu.readthedocs.io/en/master/en/extn-developer-faq/#extension-developer-faq
