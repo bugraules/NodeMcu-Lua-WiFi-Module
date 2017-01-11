4cc827db890f5150adad7d4b3d889499532f4d31
Phoenix <bugraules@gmail.com>
2017-01-11 15:18:17 +0200

    Update delay.lua

--- delay.lua
+++ delay.lua
@@ -1,3 +1,8 @@
 tmr.delay()
 --use this module to delay
 --http://nodemcu.readthedocs.io/en/master/en/modules/tmr/
+--For the asynchronous version, the minimum delay time should not be shorter than 50 μs and
+--maximum delay time is 0x7fffff μs (~8.3 seconds). In this mode the function does not block the stack and 
+--returns immediately before the output sequence is finalized. HW timer FRC1_SOURCE mode is used to change the states.
+--As there is only a single hardware timer, there are restrictions on which modules can be used at the same time. 
+--An error will be raised if the timer is already in use.
