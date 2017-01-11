e3fef6227cc8d992c2012ebe545d607f49e2ee54
Phoenix <bugraules@gmail.com>
2017-01-11 13:27:55 +0200

    Update Step1: gpio status check .lua

--- Step1: gpio status check .lua
+++ Step1: gpio status check .lua
@@ -1,19 +1,19 @@
-print(gpio.read(0))
-print(gpio.read(1))
-print(gpio.read(2))
-print(gpio.read(3))
-print(gpio.read(4))
-print(gpio.read(5))
+print(gpio.read(0))--io index 16
+print(gpio.read(1))--io index 10
+print(gpio.read(2))--io index 4
+print(gpio.read(3))--io index 9
+print(gpio.read(4))--io index 2
+print(gpio.read(5))--io index 1
 print(gpio.read(6))
-print(gpio.read(7))
+print(gpio.read(7)) 
 print(gpio.read(8))
-print(gpio.read(9))
-print(gpio.read(10))
-print(gpio.read(11))
-print(gpio.read(12))
-print(gpio.read(13))
-print(gpio.read(14))
-print(gpio.read(15))
-print(gpio.read(16))
+print(gpio.read(9))--io index 11
+print(gpio.read(10))--io index 12
+print(gpio.read(11))--io index 
+print(gpio.read(12))--io index 6
+print(gpio.read(13))--io index 7
+print(gpio.read(14))--io index 5
+print(gpio.read(15))--io index 8
+print(gpio.read(16))--io index 0[*]
 --gpio index
 --http://nodemcu.readthedocs.io/en/master/en/modules/gpio/#gpioserout
