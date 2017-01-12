6fade6f2dd0baad747dfd87273e983f2d7d6febd
Phoenix <bugraules@gmail.com>
2017-01-12 13:14:13 +0200

    Update Step2: make disable unused gpios in project .lua

--- Step2: make disable unused gpios in project .lua
+++ Step2: make disable unused gpios in project .lua
@@ -14,10 +14,8 @@ gpio.write(0,gpio.LOW)-- pin0 s blueLED is on now
 gpio.write(0,gpio.HIGH)--WOW seems pin 0 has an additional blue LED(Geekcreit Nodemcu) 
 
 --Syntax--
---gpio.mode(0,gpio.OUTPUT,gpio.PULLUP)
+--https://nodemcu.readthedocs.io/en/master/en/modules/gpio/
 --gpio.serout(pin, start_level, delay_times [, cycle_num[, callback]])
-
---EXAMPLE-- http://nodemcu.readthedocs.io/en/master/en/modules/gpio/#gpiowrite
 gpio.mode(1,gpio.OUTPUT,gpio.PULLUP)
 gpio.serout(1,1,{30,30,60,60,30,30})  -- serial one byte, b10110010
 gpio.serout(1,1,{30,70},8)  -- serial 30% pwm 10k, lasts 8 cycles
@@ -26,11 +24,11 @@ gpio.serout(1,1,{0,0},8)  -- serial 50% pwm as fast as possible, lasts 8 cycles
 gpio.serout(1,0,{20,10,10,20,10,10,10,100}) -- sim uart one byte 0x5A at about 100kbps
 gpio.serout(1,1,{8,18},8) -- serial 30% pwm 38k, lasts 8 cycles
 
+--logic ERROR part
 gpio.serout(1,1,{5000,995000},100, function() print("done") end) -- asynchronous 100 flashes 5 ms long every second with a callback function when done
-gpio.serout(1,1,{5000,995000},100, 1) -- asynchronous 100 flashes 5 ms long, no callback
+gpio.serout(1,1,{5000,995000},100, 1) -- asynchronous 100 flashes 5 ms long, no callback // nodemcu
 
-to be continueue...
-
-
+--NOTE:Nodemcu(geekcreit) can not do 100 flashes 5ms long in every second! So It restars itself! nMCU can max 2 flashes (5ms) in a sec. 
 
 
+--Synttax--
